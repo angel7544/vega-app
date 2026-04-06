8640af6ea15803e74efed434ed6e739a99286ca8
angel <angelsingh2199@gmail.com>
2026-04-06 17:25:20 +0530

    feat: add new EPG data JSON files for various Indian television channels

--- tmp/extract_epg.js
+++ tmp/extract_epg.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const inputFiles = [
-    'd:/SteamLibrary/vega-app/src/epg/epg-in.xml',
-    'd:/SteamLibrary/vega-app/src/epg/epg_1.xml',
-    'd:/SteamLibrary/vega-app/src/epg/epg.xml_2'
+    'd:/SteamLibrary/vega-app/src/epg/india.channels.xml'
 ];
 const outputDir = 'd:/SteamLibrary/vega-app/src/epg-data';
 
